898f69f70c6a004cb34bfae11b9353ffcc8d2835
JSHMorgan <JSHMorgan@outlook.com>
2023-05-19 15:49:29 +0100

    Added CheckCorrect().

--- SudokuSolver/Program.cs
+++ SudokuSolver/Program.cs
@@ -21,6 +21,7 @@ public class Program
         Console.WriteLine();
         CheckWhileLoneFound();
         PrintBoard(board);
+        Console.WriteLine(CheckCorrect());
     }
 
     private static void PopulateCells()
@@ -56,22 +57,26 @@ public class Program
     private static void CheckWhileLoneFound()
     {
         bool loneFound = true;
+        // Keep looping until no more "lone" possible numbers exist.
         while (loneFound)
         {
             loneFound = false;
+            // Check each member of the board.
             foreach (var item in board)
             {
+                // Move onto the next item if the possible numbers count is not 1.
                 if (item.PossibleNumbers.Count != 1)
                 {
                     continue;
                 }
-                loneFound = true;
+
                 item.Number = item.PossibleNumbers[0];
                 item.PossibleNumbers.RemoveAt(0);
                 foreach (var cell in item.CellComeptition)
                 {
                     cell.PossibleNumbers.Remove(item.Number);
                 }
+                loneFound = true;
             }
         }
         
@@ -121,5 +126,33 @@ public class Program
         {
             Console.WriteLine(item);
         }
+    }
+
+    private static bool CheckCorrect()
+    {
+        int[,] correctBoard = 
+        { 
+            { 5, 3, 4, 6, 7, 8, 9, 1, 2 }, 
+            { 6, 7, 2, 1, 9, 5, 3, 4, 8 }, 
+            { 1, 9, 8, 3, 4, 2, 5, 6, 7 }, 
+            { 8, 5, 9, 7, 6, 1, 4, 2, 3 }, 
+            { 4, 2, 6, 8, 5, 3, 7, 9, 1 }, 
+            { 7, 1, 3, 9, 2, 4, 8, 5, 6 }, 
+            { 9, 6, 1, 5, 3, 7, 2, 8, 4 }, 
+            { 2, 8, 7, 4, 1, 9, 6, 3, 5 }, 
+            { 3, 4, 5, 2, 8, 6, 1, 7, 9 } 
+        };
+
+        for (int i = 0; i < 9; i++)
+        {
+            for (int j = 0; j < 9; j++)
+            {
+                if (correctBoard[i,j] != board[i,j].Number)
+                {
+                    return false;
+                }
+            }
+        }
+        return true;
     }
 }
