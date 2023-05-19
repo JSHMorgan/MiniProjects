10dcfd13c1097785b357aa7e1135fef246e39fb9
JSHMorgan <JSHMorgan@outlook.com>
2023-05-19 15:27:11 +0100

    Add project files.

---
+++ SudokuSolver/Cell.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace SudokuSolver;
+internal class Cell
+{
+    internal Cell(int number, (int row, int column) position)
+    {
+        Number = number;
+        Position = position;
+        PossibleNumbers = new() { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+    }
+
+    internal int Number { get; set; } = 0;
+    internal (int row, int column) Position { get; set; }
+
+    internal List<Cell> CellComeptition { get; set; } = new();
+    internal List<int> PossibleNumbers { get; set; } = new();
+
+    internal void FindCellCompetition(Cell[,] board)
+    {
+        for (int i = 0; i < 9; i++)
+        {
+            if (i != Position.column)
+            {
+                CellComeptition.Add(board[Position.row, i]);
+            }
+            if (i != Position.row)
+            {
+                CellComeptition.Add(board[i, Position.column]);
+            }
+        }
+
+        int startRow = Position.row - Position.row % 3;
+        int startCol = Position.column - Position.column % 3;
+        for (int row = startRow; row < startRow + 3; row++)
+        {
+            for (int column = startCol; column < startCol + 3; column++)
+            {
+                if (row != Position.row && column != Position.column)
+                {
+                    CellComeptition.Add(board[row, column]);
+                }
+            }
+        }
+    }
+
+    internal void FindPossibleNumbers()
+    {
+        foreach (Cell cell in CellComeptition)
+        {
+            PossibleNumbers.Remove(cell.Number);
+        }
+    }
+}
---
+++ SudokuSolver/Program.cs
@@ -0,0 +1,99 @@
+﻿namespace SudokuSolver;
+
+public class Program
+{
+    private static readonly Cell[,] board = new Cell[9, 9];
+    /*{
+        { 5, 3, 0, 0, 7, 0, 0, 0, 0 },
+        { 6, 0, 0, 1, 9, 5, 0, 0, 0 },
+        { 0, 9, 8, 0, 0, 0, 0, 6, 0 },
+        { 8, 0, 0, 0, 6, 0, 0, 0, 3 },
+        { 4, 0, 0, 8, 0, 3, 0, 0, 1 },
+        { 7, 0, 0, 0, 2, 0, 0, 0, 6 },
+        { 0, 6, 0, 0, 0, 0, 2, 8, 0 },
+        { 0, 0, 0, 4, 1, 9, 0, 0, 5 },
+        { 0, 0, 0, 0, 8, 0, 0, 7, 9 }
+    };*/
+    public static void Main(string[] args)
+    {
+        PopulateCells();
+        PrintPossibleCells(board, board[0, 0]);
+        PrintPossibleNumbers(board[0, 0]);
+    }
+
+    private static void PopulateCells()
+    {
+        int[,] boardNums = new int[9, 9]
+        {
+            { 5, 3, 0, 0, 7, 0, 0, 0, 0 },
+            { 6, 0, 0, 1, 9, 5, 0, 0, 0 },
+            { 0, 9, 8, 0, 0, 0, 0, 6, 0 },
+            { 8, 0, 0, 0, 6, 0, 0, 0, 3 },
+            { 4, 0, 0, 8, 0, 3, 0, 0, 1 },
+            { 7, 0, 0, 0, 2, 0, 0, 0, 6 },
+            { 0, 6, 0, 0, 0, 0, 2, 8, 0 },
+            { 0, 0, 0, 4, 1, 9, 0, 0, 5 },
+            { 0, 0, 0, 0, 8, 0, 0, 7, 9 }
+        };
+        // Populate the cells with information.
+        // Have to initialize first for the position setter to work.
+        for (int row = 0;  row < board.GetLength(0); row++)
+        {
+            for (int col = 0;  col < board.GetLength(1); col++)
+            {
+                board[row, col] = new(boardNums[row, col], (row, col));
+            }
+        }
+        foreach (var cell in board)
+        {
+            cell.FindCellCompetition(board);
+            cell.FindPossibleNumbers();
+        }
+    }
+
+    private static void PrintBoard(Cell[,] board)
+    {
+        for (int row = 0; row < 9; row++)
+        {
+            for (int col = 0; col < 9; col++)
+            {
+                Console.Write($"{board[row, col].Number}    ");
+            }
+            Console.WriteLine();
+            Console.WriteLine();
+        }
+    }
+
+    private static void PrintPossibleCells(Cell[,] board, Cell cell)
+    {
+        int counter = 0;
+        foreach (var item in board)
+        {
+            if (item == cell)
+            {
+                Console.Write($"C    ");
+            }
+            else if (cell.CellComeptition.Contains(item))
+            {
+                Console.Write($"{item.Number}    ");
+            }
+            else
+            {
+                Console.Write($"N/A  ");
+            }
+            counter++;
+            if (counter % 9 == 0)
+            {
+                Console.WriteLine("\n");
+            }
+        }
+    }
+
+    private static void PrintPossibleNumbers(Cell cell)
+    {
+        foreach (var item in cell.PossibleNumbers)
+        {
+            Console.WriteLine(item);
+        }
+    }
+}
